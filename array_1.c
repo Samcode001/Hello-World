e49f7fead8c43b3d3c6af8a12f1651c16d10cd20
Samcode001 <93057570+Samcode001@users.noreply.github.com>
2021-10-25 22:22:32 +0530

    Add files via upload

---
+++ array_1.c
@@ -0,0 +1,41 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int i;
+//     int a[10] = {1, 2, 3, 4, 7, 8};
+//     for (i = 0; i < 6; i++)
+//     {
+//         printf("%d %d\n", i, a[i]);
+//     }
+//     return 0;
+// }
+
+//#include <stdio.h>
+//int main()
+//{
+//    int value[4] = {3,4,6,8};
+//    
+//    for(int i=0; i<4; i++)
+//    {
+//        printf("%d %d\n",i, value[i]);
+//        scanf("%d", &value[i]);
+//        
+//    }
+//    return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//    int value[6];
+//    printf("Enter sixinteger :\n");
+//    for(int i=0; i<6; i++)
+//    {
+//        scanf("%d", &value[i]);
+//    }
+//    return 0;
+//}
+
+// #include<stdio.h>
+// int main()
+// {
